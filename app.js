40cc6886bba9c1889b5362e07d8a6456baa20c14
panamacheng <rangotoptalent@gmail.com>
2020-11-26 08:06:21 -0800

    updated code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http');
-const TikTokScraper = require('./build');
+const TikTokScraper = require('tiktok-scraper');
 
 const app = express()
 const port = process.env.PORT || 5000;
